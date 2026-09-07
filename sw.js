91f478797fb3ba7407bc40b3d78e305c8c9e1c1b
Ma petite madeleine <editionsoulitka@gmail.com>
2026-09-07 14:48:13 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mpm-v10';
+const CACHE = 'mpm-v12';
 const SHELL = [
   './index.html','./styles.css','./_ds_bundle.js','./manifest.webmanifest',
   './app/exercices.js','./app/mpm-app.jsx',
